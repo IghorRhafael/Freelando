8a5f59f33ee283e673184938985b591dad75ac44
Ighor Rhafael <ighorrhafael@hotmail.com>
2025-02-10 13:24:39 +0000

    Adicionar arquivos de projeto.

---
+++ src/Freelando.Dados/FreelandoContext.cs
@@ -0,0 +1,31 @@
+﻿using Freelando.Modelo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Freelando.Dados;
+
+public class FreelandoContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public FreelandoContext(DbContextOptions<FreelandoContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ConnectionStrings:DefaultConnection"));
+        }
+    }
+
+    public DbSet<Candidatura> Candidaturas { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Contrato> Contratos { get; set; }
+    public DbSet<Especialidade> Especialidades { get; set; }
+    public DbSet<Profissional> profissionais { get; set; }
+    public DbSet<Projeto> Projetos { get; set; }
+    public DbSet<Servico> Servicos { get; set; }
+
+}
